54c5664d5584fb5a616e6528123213d8506c2275
Jens L. Nedregård <jensln@uio.no>
2020-12-06 14:43:16 +0100

    Add helper for reading sections of input separated by newline

--- helper.py
+++ helper.py
@@ -1,11 +1,30 @@
 import os, sys
 
-def lines_from_day(n):
-    fname = f"data/day{n}.txt"
+def buildlist(f, *its):
+    return list(map(f, *its))
 
+def _ensure_existance(fname):
     if not os.path.exists(fname):
         print(f"Data-file {fname} does not exist!\nPlace data here, then re-run.")
-    else:
+        sys.exit(1)
+    return True
+
+def lines_from_day(n):
+    fname = f"data/day{n}.txt"
+    if _ensure_existance(fname):
         with open(fname) as infile:
             for line in infile:
                 yield line.strip()
+
+def sections_from_day(n):
+    sections = []; acc = []
+    for line in lines_from_day(n):
+        if bool(line.strip()): # line not blank
+            acc.append(line.strip())
+        else:
+            sections.append(acc)
+            acc = []
+    if acc: # the last line might not be blank
+        sections.append(acc)
+    return sections
+
