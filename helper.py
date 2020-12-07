69f5f5ef0754298cd751e0abe00933d0ece9700c
Jens L. Nedregård <jensln@uio.no>
2020-12-07 13:54:37 +0100

    Add `alt` option to helper

--- helper.py
+++ helper.py
@@ -9,16 +9,16 @@ def _ensure_existance(fname):
         sys.exit(1)
     return True
 
-def lines_from_day(n):
-    fname = f"data/day{n}.txt"
+def lines_from_day(n, alt=""):
+    fname = f"data/day{n}{'-' if alt else ''}{alt}.txt"
     if _ensure_existance(fname):
         with open(fname) as infile:
             for line in infile:
                 yield line.strip()
 
-def sections_from_day(n, concat=False, split=None):
+def sections_from_day(n, concat=False, split=None, alt=""):
     sections = []; acc = []
-    for line in lines_from_day(n):
+    for line in lines_from_day(n, alt):
         if bool(line): # line not blank
             l = line.split(split) if split else line
             if concat: acc += l
